e39b4565184dbc33d0608104a902ef348246e8c9
niklasbaumgardner <niklasbaumgardner@gmail.com>
2025-06-13 14:21:04 -0400

    remove backend

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,17 +3,12 @@
 set -euo pipefail
 
 # for backwards compatibility, seperates host and port from url
-export FRONTEND_DOMAIN=${FRONTEND_DOMAIN:-${FRONTEND_HOST%:*}}
-export FRONTEND_PORT=${FRONTEND_PORT:-${FRONTEND_HOST##*:}}
-
-export BACKEND_DOMAIN=${BACKEND_DOMAIN:-${BACKEND_HOST%:*}}
-export BACKEND_PORT=${BACKEND_PORT:-${BACKEND_HOST##*:}}
+export APP_DOMAIN=${APP_DOMAIN:-${FRONTEND_HOST%:*}}
+export APP_PORT=${APP_PORT:-${FRONTEND_HOST##*:}}
 
 # strip https:// or https:// from domain if necessary
-FRONTEND_DOMAIN=${FRONTEND_DOMAIN##*://}
-BACKEND_DOMAIN=${BACKEND_DOMAIN##*://}
+APP_DOMAIN=${APP_DOMAIN##*://}
 
-echo using frontend: ${FRONTEND_DOMAIN} with port: ${FRONTEND_PORT}
-echo using backend: ${BACKEND_DOMAIN} with port: ${BACKEND_PORT}
+echo using app: ${APP_DOMAIN} with port: ${APP_PORT}
 
 exec caddy run --config Caddyfile --adapter caddyfile 2>&1
